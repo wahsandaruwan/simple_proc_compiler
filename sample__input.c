d21da19d1e829d1afd1293c83597e3971e30cc7f
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-06-30 00:24:16 +0530

    Code updaed

--- sample__input.c
+++ sample__input.c
@@ -2,6 +2,12 @@
 #include<string.h>
 
 int startFunc() {
+    /* 
+        This is our sample
+        multiline comment
+    */
+    // This is our sample single line comment
+
     int a;
     int x=1;
     int y=2;
@@ -9,6 +15,7 @@ int startFunc() {
     x=3;
     y=10;
     z=5;
+
     if(x>5) {
         for(int k=0; k<10; k++) {
             y = x+3;
@@ -17,12 +24,15 @@ int startFunc() {
     } else {
         int idx = 1;
     }
+
     for(int i=0; i<10; i++) {
         print("Hello Himal!");
         scan("%d", &x);
+
         if (x>5) {
             print("Hi");
         }
+        
         for(int j=0; j<z; j++) {
             a=1;
         }
